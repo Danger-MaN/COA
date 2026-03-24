0c7037bcbce32cfdcba310426c893a104dabf3a2
Mohamed Magdi <30964819+Danger-MaN@users.noreply.github.com>
2026-03-24 18:57:12 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { VotesProvider } from "@/context/VotesContext";
 import Index from "./pages/Index.tsx";
 import GenderSelect from "./pages/GenderSelect.tsx";
 import VotingPage from "./pages/VotingPage.tsx";
@@ -14,17 +15,19 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/select" element={<GenderSelect />} />
-          <Route path="/vote/:gender" element={<VotingPage />} />
-          <Route path="/candidate/:id" element={<CandidatePage />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <VotesProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/select" element={<GenderSelect />} />
+            <Route path="/vote/:gender" element={<VotingPage />} />
+            <Route path="/candidate/:id" element={<CandidatePage />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </VotesProvider>
     </TooltipProvider>
   </QueryClientProvider>
 );
